447274f526278f7b9d4a6b85c4b3b5661d7afdba
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-15 16:09:27 -0500

    update auth

--- src/lib/auth/local/index.js
+++ src/lib/auth/local/index.js
@@ -9,10 +9,11 @@ router.post('/', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
 
     const error = err || info;
+
     if (error)
-      return res.status(400).json(error);
+      return res.status(401).json(error);
     if (!user)
-      return res.status(404).json({
+      return res.status(401).json({
         error: 'Something went wrong, please try again.'
       });
 
