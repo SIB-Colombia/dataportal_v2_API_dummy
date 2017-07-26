533b36cfaad6b7ebc062b4b32b85cf9cfeba2aca
Miguel Morales <mmorales@idesoluciones.com>
2017-07-26 15:17:13 -0500

    modificado:    src/api/entities/entities.controller.js 	modificado:    src/api/entities/index.js

--- src/api/entities/index.js
+++ src/api/entities/index.js
@@ -4,5 +4,6 @@ import * as controller from './entities.controller';
 const router = express.Router();
 
 router.get('/basic', controller.read);
+router.get('/list', controller.list);
 
 export default router;
