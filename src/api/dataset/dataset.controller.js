7c0733097a183f9444bcd4d0dc1a177339629ccb
Miguel Morales <mmorales@idesoluciones.com>
2017-07-28 09:47:38 -0500

    se agregan campo necesario para el response

--- src/api/dataset/dataset.controller.js
+++ src/api/dataset/dataset.controller.js
@@ -1,48 +1,50 @@
-export function read(req, res) {
+export function list(req, res) {
 
   var jsonResponse = [
     {
       "resourceId": 1,
       "resourceName": "EOD - eBird Observation Dataset",
       "providerName": "Cornell Lab of Ornithology",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem hic eligendi inventore labore ad aliquam sed, id, tempore eveniet natus. Necessitatibus suscipit veritatis reiciendis inventore, in sint aut porro perferendis.',
       "collectionName": "",
       "institutionCode": "",
-      "count": 275718645
+      location: 'CIUDAD',
+      "count": 275718645,
+      imageUrl: "/logo_entidad.png",
+      type: 'ocurrencce dataset'
     },
     {
-      "resourceId": 1,
+      "resourceId": 2,
       "resourceName": "EOD - eBird Observation Dataset",
       "providerName": "Cornell Lab of Ornithology",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem hic eligendi inventore labore ad aliquam sed, id, tempore eveniet natus. Necessitatibus suscipit veritatis reiciendis inventore, in sint aut porro perferendis.',
       "collectionName": "",
       "institutionCode": "",
-      "count": 275718645
-    },
-    {
-      "resourceId": 1,
-      "resourceName": "EOD - eBird Observation Dataset",
-      "providerName": "Cornell Lab of Ornithology",
-      "collectionName": "",
-      "institutionCode": "",
-      "count": 275718645
-    },
-    {
-      "resourceId": 1,
-      "resourceName": "EOD - eBird Observation Dataset",
-      "providerName": "Cornell Lab of Ornithology",
-      "collectionName": "",
-      "institutionCode": "",
-      "count": 275718645
-    },
-    {
-      "resourceId": 1,
-      "resourceName": "EOD - eBird Observation Dataset",
-      "providerName": "Cornell Lab of Ornithology",
-      "collectionName": "",
-      "institutionCode": "",
-      "count": 275718645
+      location: 'CIUDAD',
+      "count": 275718645,
+      imageUrl: "/logo_entidad.png",
+      type: 'ocurrencce dataset'
     }
   ]
 
   return res.status(200).json(jsonResponse);
 
+}
+
+export function read(req, res) {
+
+  var jsonResponse = {
+    "resourceId": 1,
+    "resourceName": "EOD - eBird Observation Dataset",
+    "providerName": "Cornell Lab of Ornithology",
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem hic eligendi inventore labore ad aliquam sed, id, tempore eveniet natus. Necessitatibus suscipit veritatis reiciendis inventore, in sint aut porro perferendis.',
+    "collectionName": "",
+    "institutionCode": "",
+    location: 'CIUDAD',
+    "count": 275718645,
+    imageUrl: "/logo_entidad.png",
+    type: 'ocurrencce dataset'
+  }
+
+  return res.status(200).json(jsonResponse);
 }
--- src/api/dataset/index.js
+++ src/api/dataset/index.js
@@ -4,5 +4,6 @@ import * as controller from './dataset.controller';
 const router = express.Router();
 
 router.get('/basic', controller.read)
+router.get('/list', controller.list)
 
 export default router;
