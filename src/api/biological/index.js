c64cb0ce9a8685caf53769f758eab4ea441b0b4d
Jorge Gonzalez <jag2kn@gmail.com>
2017-10-20 16:24:51 -0500

    Se integra el llamado a gbif en el servicio

--- src/api/biological/index.js
+++ src/api/biological/index.js
@@ -4,5 +4,6 @@ import * as controller from './biological.controller';
 const router = express.Router();
 
 router.get('/basic', controller.read);
+router.get('/basic/:id', controller.read);
 
 export default router;
